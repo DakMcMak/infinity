471c17bfd49d84fe037c9b45b45eb633b98d0038
DakMcMak <esbolov.galymzhan@mail.ru>
2017-04-05 23:39:41 +0600

    Initial commit

---
+++ src/main/java/infinity.java
@@ -0,0 +1,9 @@
+
+public class infinity {
+    public static void main(String[] args) {
+        while (true)
+            System.out.println(" eat " +
+                    " programming " +
+                    " sleep ");
+    }
+}
